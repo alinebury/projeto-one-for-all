143e9d4dcc1971f3e0eb821febdb7d438851977a
Aline Bury <alinebury@gmail.com>
2022-06-06 15:25:27 -0300

    Req 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,13 +1,12 @@
 SELECT 
     artist.artist_name AS 'artista',
     album.album_name AS 'album',
-	COUNT(follow.artist_id) AS 'seguidores'
+    COUNT(follow.artist_id) AS 'seguidores'
 FROM
     SpotifyClone.artist AS artist
         INNER JOIN
-    SpotifyClone.album AS album
-ON artist.id = album.artist_id
-		INNER JOIN
-	SpotifyClone.followers AS follow
-ON artist.id = follow.artist_id
-GROUP BY follow.artist_id, album.album_name;
+    SpotifyClone.album AS album ON artist.id = album.artist_id
+        INNER JOIN
+    SpotifyClone.followers AS follow ON artist.id = follow.artist_id
+GROUP BY follow.artist_id , album.album_name
+ORDER BY follow.artist_id , artist.artist_name , album.album_name;
